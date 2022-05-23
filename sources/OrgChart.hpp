3bc73a4f2d876f5be98d3bfa15c3043c2c55a600
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-23 17:33:14 +0300

    finish

--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -1,9 +1,9 @@
 #pragma once
+#include <string>
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
 #include <deque>
+#include <vector>
 using namespace std;
 
 
@@ -17,40 +17,39 @@ namespace ariel
 
     class OrgChart
     {
-    private:
-        Node root;
-        bool find_sub(Node &node, string &father, string &son);
-        void fill_q_for_level_order(Node &node);
-        void fill_q_for_print_level_order(Node &node, string s, bool isLast);
-        void fill_q_for_reverse_order(Node &node);
-        void fill_q_for_preorder(Node &node);
-        vector<string> b_level_order;
-        vector<string> b_print_level_order;
-        vector<string> b_reverse_order;
-        vector<string> b_preorder;
+    
 
     public:
         OrgChart();
-        OrgChart &add_root(string name);
+        ~OrgChart();
         OrgChart &add_sub(string father, string son);
-
+        OrgChart &add_root(string name);
         friend ostream &operator<<(ostream &out,OrgChart &root);
-        string* begin_level_order();
-        string* end_level_order();
-        string* begin_print_level_order();
-        string* end_print_level_order(); 
-        string* begin_reverse_order();
-        string* reverse_order();
         string* begin_preorder();
+        string* end_print_level_order(); 
+        string* reverse_order();
+        string* begin_level_order();
+        string* begin_reverse_order();
         string* end_preorder();
+        string* begin_print_level_order();
+        string* end_level_order();
         string* begin(){
             return begin_level_order();
             }
         string* end(){
             return end_level_order();
             }
-
-        ~OrgChart();
+    private:
+        vector<string> b_print_level_order;
+        vector<string> b_preorder;
+        vector<string> b_reverse_order;
+        vector<string> b_level_order;
+        Node root;
+        void fill_q_for_print_level_order(Node &node, string s, bool isLast);
+        bool find_sub(Node &node, string &father, string &son);
+        void fill_q_for_reverse_order(Node &node);
+        void fill_q_for_preorder(Node &node);
+        void fill_q_for_level_order(Node &node);
         };
 
 }
